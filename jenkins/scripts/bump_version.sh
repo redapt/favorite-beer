cbfeb511a51c285862222ff0d02e41108fb42dee
jmeisner3 <jmeisner@redapt.com>
2022-07-14 21:59:22 -0700

    Adding some more jenkins pipeline example files.

---
+++ jenkins/scripts/bump_version.sh
@@ -0,0 +1,15 @@
+export currentBranch=$(git branch --show-current)
+git checkout -b release
+export VERSION=$(cat VERSION)
+VERSION="${VERSION#[vV]}"
+VERSION_MAJOR="${VERSION%%\.*}"
+VERSION_MINOR="${VERSION#*.}"
+VERSION_MINOR="${VERSION_MINOR%.*}"
+VERSION_PATCH="${VERSION##*.}"
+
+echo "Version: ${VERSION}"
+echo "Version [major]: ${VERSION_MAJOR}"
+echo "Version [minor]: ${VERSION_MINOR}"
+echo "Version [patch]: ${VERSION_PATCH}"
+
+git checkout $currentBranch
